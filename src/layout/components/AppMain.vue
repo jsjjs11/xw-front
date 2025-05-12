65df1d2e8273172984754bbed008f7af074f164a
俞铭 <1052621377@qq.com>
2025-05-12 09:46:58 +0800

    Revert "app-main 高度修改"
    This reverts commit 4779686a

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -41,9 +41,8 @@ export default {
 
 .hasTagsView {
   .app-main {
-    /* 84 = navbar + tags-view = 50 + 36 */
-    min-height: calc(100vh - 86px);
-    height: calc(100vh - 86px);
+    /* 84 = navbar + tags-view = 50 + 34 */
+    min-height: calc(100vh - 84px);
   }
 
   .fixed-header + .app-main {
