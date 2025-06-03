9e689758038d8e422935f46a01caa625ef7ff8b5
俞铭 <1052621377@qq.com>
2025-06-03 14:39:34 +0800

    授权审核

---
+++ src/views/nacs/check/checkForm.vue
@@ -0,0 +1,149 @@
+<template>
+	<div>
+		<!-- 审核对话框 -->
+    <el-dialog
+      title="审核处理"
+      :visible.sync="auditDialogVisible"
+      width="600px"
+			ref="formRef">
+      <el-form label-width="100px">
+        <el-form-item label="审核意见">
+          <el-input
+            type="textarea"
+            :rows="3"
+            v-model="currentAudit.remark"
+            placeholder="请输入审核意见"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="auditDialogVisible = false">取 消</el-button>
+        <el-button type="danger" @click="handleReject">驳 回</el-button>
+        <el-button type="success" @click="handleApprove">通 过</el-button>
+      </span>
+    </el-dialog>
+
+    <!-- 驳回原因对话框 -->
+    <!-- <el-dialog
+      title="填写驳回原因"
+      :visible.sync="rejectDialogVisible"
+      width="500px">
+      <el-form :model="rejectForm" :rules="rejectRules" ref="rejectForm">
+        <el-form-item label="驳回原因" prop="reason">
+          <el-input
+            type="textarea"
+            :rows="3"
+            v-model="rejectForm.reason"
+            placeholder="请输入详细的驳回原因"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="rejectDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="handleReject">确 定</el-button>
+      </span>
+    </el-dialog> -->
+	</div>
+</template>
+<script>
+import * as CheckApi from '@/api/nacs/check'
+export default {
+	name: 'checkForm',
+	data() {
+		return {
+			auditDialogVisible: false,
+      // rejectDialogVisible: false,
+      currentAudit: {},
+      // rejectForm: {
+      //   reason: ''
+      // },
+      // rejectRules: {
+      //   reason: [
+      //     { required: true, message: '请输入驳回原因', trigger: 'blur' }
+      //   ]
+      // },
+      currentAudit: {
+        remark: '',
+      },
+      params:{
+        id: 0,
+        idCard: "",
+        authNo: "",
+        reviewState: 0,
+        reviewContent: "",
+        leaderUserId: "",
+        remark: ""
+      },
+		}
+	},
+	methods: {
+		open(data) {
+      this.auditDialogVisible = true;
+      this.params.id = data.id;
+      this.params.idCard = data.idCard;
+      this.params.authNo = data.authNo;
+      this.params.reviewState = data.reviewState;
+      this.params.reviewContent = data.reviewContent;
+      const userId = this.$store.getters.userId;
+      this.params.leaderUserId = userId;
+      console.log(this.params)
+		},
+		showRejectDialog() {
+      this.auditDialogVisible = false
+      this.rejectDialogVisible = true
+    },
+		handleApprove() {
+      this.$confirm('确认通过该申请吗？', '提示', {
+        type: 'warning'
+      }).then(async () => {
+        this.params.remark = this.currentAudit.remark;
+        try {
+          this.params.reviewState = 1;
+          await CheckApi.reviewCheck(this.params)
+          this.$emit('success', {
+            authNo: this.params.authNo,
+            reviewState: 1 // 1表示已通过
+          });
+          this.auditDialogVisible = false;
+          this.$modal.msgSuccess("审核通过成功");
+        } catch (error) {
+          this.$message.error('审核失败，请重试')
+        }
+        // this.updateStatus('1', '审核通过成功')
+      })
+    },
+		handleReject() {
+      this.$confirm('确认驳回该申请吗？', '提示', {
+        type: 'warning'
+      }).then(async () => {
+        this.params.remark = this.currentAudit.remark;
+        try {
+          this.params.reviewState = 2;
+          await CheckApi.reviewCheck(this.params);
+          this.$emit('success', {
+            authNo: this.params.authNo,
+            reviewState: 2 // 1表示已通过
+          });
+          this.auditDialogVisible = false;
+          this.$modal.msgSuccess("审核已驳回");
+        } catch (error) {
+          this.$message.error('审核失败，请重试')
+        }
+        // this.updateStatus('2', '驳回成功')
+      })
+    },
+		// updateStatus(status, msg) {
+    //   const index = this.tableData.findIndex(
+    //     item => item.authNo === this.currentAudit.authNo
+    //   )
+    //   if (index > -1) {
+    //     this.tableData[index].checkStatus = status
+    //     if (status === '2') {
+    //       this.tableData[index].rejectReason = this.rejectForm.reason
+    //     }
+    //     this.$message.success(msg)
+    //     this.auditDialogVisible = false
+    //     // this.rejectDialogVisible = false
+    //   }
+    // },
+	}
+}
+</script>
---
+++ src/views/nacs/check/index.vue
@@ -0,0 +1,260 @@
+<template>
+	<div class="app-container">
+		<!-- 查询表单 -->
+		<el-form :inline="true" :model="queryParams" size="small" ref="queryForm" class="search-form">
+			<el-form-item label="审核单号">
+				<el-input v-model="queryParams.authNo" placeholder="请输入审核单号" clearable
+					@keyup.enter.native="handleSearch"></el-input>
+			</el-form-item>
+			<!-- <el-form-item label="发起人">
+				<el-input v-model="queryParams.checkUser" placeholder="请输入发起人"></el-input>
+			</el-form-item> -->
+			<el-form-item label="负责人">
+				<el-input v-model="queryParams.leaderUserId" placeholder="请输入负责人" clearable
+					@keyup.enter.native="handleSearch"></el-input>
+			</el-form-item>
+			<el-form-item label="审核状态">
+				<el-select v-model="queryParams.reviewState" placeholder="请选择审核状态" clearable
+					@keyup.enter.native="handleSearch">
+					<el-option v-for="dict in checkStateDictDatas" :key="parseInt(dict.value)" :label="dict.label"
+						:value="parseInt(dict.value)"></el-option>
+				</el-select>
+			</el-form-item>
+			<el-form-item label="发起时间">
+				<el-date-picker v-model="queryParams.createTime" type="daterange" range-separator="至"
+					start-placeholder="开始日期" end-placeholder="结束日期" value-format="yyyy-MM-dd HH:mm:ss"
+					:default-time="['00:00:00', '23:59:59']">
+				</el-date-picker>
+			</el-form-item>
+			<el-form-item>
+				<el-button type="primary" icon="el-icon-search" @click="handleSearch">搜索</el-button>
+				<el-button icon="el-icon-refresh" @click="handleReset">重置</el-button>
+			</el-form-item>
+		</el-form>
+		<div class="table-container">
+			<el-table ref="tableRef" :data="list" style="width: 100%; height: 655px;" v-loading="loading" :stripe="true"
+				:show-overflow-tooltip="true" row-key="authNo" height="calc(100vh - 220px)">
+				<el-table-column type="selection" width="45" align="center" :reserve-selection="true"></el-table-column>
+				<el-table-column type="index" width="40" align="center" />
+				<el-table-column label="审核单号" align="center" prop="authNo"></el-table-column>
+				<el-table-column label="审核内容" align="center" prop="reviewContent"></el-table-column>
+				<el-table-column label="发起时间" align="center" prop="createTime"></el-table-column>
+				<!-- <el-table-column label="发起人" align="center" prop="checkUser"></el-table-column> -->
+				<el-table-column label="审核状态" align="center" prop="reviewState">
+					<template slot-scope="scope">
+						<dict-tag :type="DICT_TYPE.NACS_CHECK_STATE" :value="scope.row.reviewState" />
+					</template>
+				</el-table-column>
+				<el-table-column label="负责人" align="center" prop="leaderUserName"></el-table-column>
+				<el-table-column label="操作" align="center" width="250">
+					<template slot-scope="scope">
+						<el-button type="text" @click.stop="handleView(scope.row)"
+							:icon="activeRow === scope.row.authNo ? 'el-icon-arrow-up' : 'el-icon-arrow-down'">{{
+							activeRow === scope.row.authNo ? '收起' : '详情' }}</el-button>
+						<el-button type="text" :disabled="scope.row.reviewState !== 0" @click="handleCheck(scope.row)"
+							v-hasPermi="['nacs:authorize:check']">{{scope.row.reviewState === 0 ? '审核' :
+							'已处理'}}</el-button>
+					</template>
+				</el-table-column>
+
+				<!-- 展开详情 -->
+				<el-table-column type="expand" width="1">
+					<template #default="{ row }">
+						<div v-show="activeRow === row.authNo" class="expand-content">
+							<el-table :data="row.detailData" border>
+								<el-table-column label="ID" prop="id" align="center" width="80"></el-table-column>
+								<el-table-column label="线路名称" prop="lineNo" align="center" width="100">
+									<template v-slot="scope">
+										<span>{{lineList.find(line => line.lineNo === scope.row.lineNo).name}}</span>
+									</template>
+								</el-table-column>
+								<el-table-column label="站点编号" prop="stationNo" align="center"
+									width="100"></el-table-column>
+								<el-table-column label="身份证号" prop="idCard" align="center"
+									width="180"></el-table-column>
+								<el-table-column label="员工姓名" prop="employeeName" align="center"
+									width="100"></el-table-column>
+								<el-table-column label="卡号" prop="cardNo" align="center" width="120"></el-table-column>
+								<el-table-column label="区域代码" prop="code" align="center" width="100"></el-table-column>
+								<el-table-column label="区域名称" prop="name" align="center"></el-table-column>
+								<el-table-column label="授权方式" prop="authMode" align="center" width="100">
+									<template slot-scope="scope">
+										<el-tag :type="scope.row.authMode === 2 ? 'success' : 'info'">
+											{{ scope.row.authMode === 2 ? '刷卡' : '其他' }}
+										</el-tag>
+									</template>
+								</el-table-column>
+								<el-table-column label="授权单号" prop="authNo" align="center"
+									width="180"></el-table-column>
+								<el-table-column label="创建时间" prop="createTime" align="center" width="180">
+									<template slot-scope="scope">
+										{{ formatTimestamp(scope.row.createTime) }}
+									</template>
+								</el-table-column>
+							</el-table>
+						</div>
+					</template>
+				</el-table-column>
+
+			</el-table>
+			<pagination v-show="total>0" :total="total" :page.sync="queryParams.pageNo"
+				:limit.sync="queryParams.pageSize" @pagination="getList" style="height: 50px;" />
+		</div>
+		<CheckForm ref="formRef" @success="getList" />
+		<CheckForm ref="formRef" @success="handleCheckSuccess" />
+	</div>
+</template>
+<script>
+import CheckForm from './checkForm.vue'
+import * as CheckApi from '@/api/nacs/check'
+import { DICT_TYPE, getDictDatas, getLineDatas } from "@/utils/dict";
+export default {
+	name: 'CheckIndex',
+	components: {
+		CheckForm
+	},
+	// created() {
+	// 	this.getList();
+	// },
+	data() {
+		return {
+			lineList: getLineDatas(),
+			list: [],
+			total: 3,
+			loading: false,
+			queryParams: {
+				pageNo: 1,
+				pageSize: 10,
+				authNo: null,
+				leaderUserName: null,
+        	reviewState: null,
+        	createTime: []
+			},
+			checkStateDictDatas: getDictDatas(DICT_TYPE.NACS_CHECK_STATE),
+			activeRow: null  // 当前展开行的id
+		}
+	},
+	mounted() {
+		this.getList();
+	},
+	methods: {
+		/** 查询列表 */
+    async getList() {
+      try {
+      	this.loading = true;
+				const res = await CheckApi.getCheckPage(this.queryParams);
+				if (res.data.list) {
+					res.data.list.map(item => item.createTime = this.formatTimestamp(item.createTime))
+				}
+				this.list = res.data.list;
+				this.total = res.data.total;
+      } finally {
+        this.loading = false;
+      }
+    },
+		formatTimestamp(timestamp) {
+			const date = new Date(timestamp);
+			const year = date.getFullYear();
+			const month = String(date.getMonth() + 1).padStart(2, '0'); // 月份从0开始需+1
+			const day = String(date.getDate()).padStart(2, '0');
+			const hours = String(date.getHours()).padStart(2, '0');
+			const minutes = String(date.getMinutes()).padStart(2, '0');
+			const seconds = String(date.getSeconds()).padStart(2, '0');
+			return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+		},
+		/** 搜索按钮操作 */
+    handleSearch() {
+      this.queryParams.pageNo = 1;
+      this.getList();
+    },
+    /** 重置按钮操作 */
+    handleReset() {
+      this.resetForm("queryForm");
+			this.queryParams = {
+        pageNo: 1,
+        pageSize: 10,
+        authNo: null,
+        leaderUserName: null,
+        reviewState: null,
+        createTime: []
+      };
+      this.handleSearch();
+    },
+		/** 详情按钮操作 */
+		async handleView(row) {
+			try {
+				if (this.activeRow === row.authNo) {
+					this.activeRow = null;
+					return;
+				}
+				this.loading = true;
+				// 调用API获取详情数据
+				const res = await CheckApi.getCheckDetail(row.authNo);
+				// 合并详情数据到当前行
+				this.list = this.list.map(item => {
+					if (item.authNo === row.authNo) {
+						return {
+							...item,
+							detailData: [...res.data]
+						};
+					}
+					return item;
+				});
+				this.activeRow = row.authNo;
+				this.$nextTick(() => {
+					this.$refs.tableRef.toggleRowExpansion(row, true);
+				})
+			} catch (error) {
+				console.error("获取详情失败", error);
+				this.$modal.msgError("获取详情失败");
+			} finally {
+				this.loading = false;
+			}
+		},
+		/** 审核按钮操作 */
+		handleCheck(row) {
+			this.$refs["formRef"].open(row);
+			this.getList();
+		},
+		/** 审核成功回调 */
+		handleCheckSuccess({authNo, reviewState}) {
+			this.list = this.list.map(item => {
+				if (item.authNo === authNo) {
+					return {
+						...item,
+						reviewState
+					};
+				}
+				return item;
+			});
+		}
+	}
+}
+</script>
+<style lang="scss" scoped>
+::v-deep .el-table {
+	// 展开行样式
+	.expanded-row {
+		background: #f8fafc !important;
+		height: 0;
+		overflow: hidden;
+		td {
+			border-bottom: 0 !important;
+			padding: 0 !important;
+		}
+	}
+
+	// 展开内容区域
+	.expand-content {
+		padding: 40px 40px;
+		background: #f8fafc;
+		border-top: 1px solid #ebeef5;
+		max-height: 400px;
+  		overflow-y: auto;
+		margin: 0;
+	}
+}
+::v-deep .el-table__expanded-cell {
+	padding: 0px !important;
+}
+</style>
